e4fa6ec0c131c766ed7ef35f17337fff057c4093
kip <kkipngenokoech22@gmail.com>
2023-05-04 15:17:33 +0300

    npm init

--- REACT/AdoptMe/README.md
+++ REACT/AdoptMe/README.md
@@ -7,3 +7,17 @@ we make an component called App.
 
 react functional components must return a markup - `returned by React.CreateElement`
 
+## props
+
+these are variables that are passed from a parent component to a child component 
+
+you pass props as an argument.
+
+so to access the props in the child component you: `props.AttributeName`
+
+to pass this props in the parent component where you call the component you: `<ChildComponent props.AttributeName = "attribute value" />`
+## npm 
+
+npm allows you to bring in code from the npm registry which is a bunch of open source modules that people have written so you can use them in your project.
+
+to start an npm project in your react project you run `npm init`
--- REACT/AdoptMe/src/App.js
+++ REACT/AdoptMe/src/App.js
@@ -1,15 +1,26 @@
-const Pet = () => {
-    return React.containerElement
+function Pet (props){
+    return (
+        <div>
+            <h1>{props.name}</h1>
+            <h2>{props.animal}</h2>
+            <h3>{props.breed}</h3>
+        </div>
+    )
 }
 
 
-const App = () => {
-        return React.CreateElement(
-            "div",
-            {},
-            React.CreateElement("h1", {}, "Adopt Me")
-        )
-    }
-const container = document.getElementById("root")
-const root = ReactDOM.createRoot(container)
-root.render(React.CreateElement(App))
+function App(){
+    return (
+        <div>
+            <h1> Adopt Me</h1>
+            <Pet name = "jimmy" animal = "Dog" breed = "Havanesse"/>
+            <Pet/>
+            <Pet/>
+        </div>
+    )
+}
+// const container = document.getElementById("root")
+// const root = ReactDOM.createRoot(container)
+// root.render(<App />)
+
+ReactDOM.render(<App />, document.getElementById("root"))
